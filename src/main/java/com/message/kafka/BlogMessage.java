c0757722cced044960c45abba34e305b39d3b9c3
zhuqingchun <729512117@qq.com>
2019-11-23 21:20:28 +0800

    项目结束

--- src/main/java/com/message/kafka/BlogMessage.java
+++ src/main/java/com/message/kafka/BlogMessage.java
@@ -11,10 +11,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 
 @Component
-public class BlogMessage {
+public class BlogMessage{
 
     private static final Logger logger= LoggerFactory.getLogger(BlogMessage.class);
 
@@ -26,16 +25,19 @@ public class BlogMessage {
 
     @KafkaListener(topics = "blogSend",groupId = "blogGroup")
     public void listenKafka(ConsumerRecord<String,String> consumerRecord){
-        //监听kafka topic blogSend
-        String key=consumerRecord.key();
-        String value=consumerRecord.value();
-        String topic=consumerRecord.topic();
-        Message message= JsonUtil.stringToObj(value,Message.class);
-        messageMapper.insert(message);
-        logger.info("key"+key);
-        logger.info("value"+value);
-        logger.info("topic"+topic);
-        //处理完成后，在将信息发送的kafka topic blogGet
-        kafkaTemplate.send("blogGet",key,value);
+        new Thread(() -> {
+            //监听kafka topic blogSend
+            String key=consumerRecord.key();
+            String value=consumerRecord.value();
+            String topic=consumerRecord.topic();
+            Message message= JsonUtil.stringToObj(value,Message.class);
+            messageMapper.insert(message);
+            logger.info("key"+key);
+            logger.info("value"+value);
+            logger.info("topic"+topic);
+            //处理完成后，在将信息发送的kafka topic blogGet
+            kafkaTemplate.send("blogGet",key,value);
+        }).start();
     }
+
 }
